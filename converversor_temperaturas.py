4e6fb055509cc275aafe0c278b928133ef318a9b
Guilherme Henrique <guilhermehcma@gmail.com>
2024-06-19 18:22:26 -0300

    Main Converter

--- converversor_temperaturas.py
+++ converversor_temperaturas.py
@@ -2,24 +2,10 @@ class ConversorTemperatura:
     def __init__(self):
         pass
 
-    def celsius_to_fahrenheit(celsius):
-     return (celsius * 9/5) + 32
+    def celsius_to_fahrenheit(valor):
+     return (valor * 9/5) + 32
 
-    def fahrenheit_to_celsius(fahrenheit):
-     return (fahrenheit - 32) * 5/9
+    def fahrenheit_to_celsius(valor):
+     return (valor - 32) * 5/9
 
-    print("Escolha a conversão que deseja realizar:")
-    print("1: Celsius para Fahrenheit")
-    print("2: Fahrenheit para Celsius")
-    escolha = input("Digite 1 ou 2: ")
-
-    if escolha == "1":
-        celsius = float(input("Digite a temperatura em Celsius: "))
-        fahrenheit = celsius_to_fahrenheit(celsius)
-        print(f"{celsius} graus Celsius é igual a {fahrenheit} graus Fahrenheit.")
-    elif escolha == "2":
-        fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
-        celsius = fahrenheit_to_celsius(fahrenheit)
-        print(f"{fahrenheit} graus Fahrenheit é igual a {celsius} graus Celsius.")
-    else:
-        print("Escolha inválida. Por favor, digite 1 ou 2.")
+  
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+from conversor_medidas import ConversorMedidas
+from converversor_temperaturas import ConversorTemperatura
 def menu_principal():
-    print("\n**Calculadora de Conversão de Unidades**")
-    print("1. Converter Comprimento")
-    print("2. Converter Temperatura")
-    print("3. Sair")
-    opção = (input("Digite a opção desejada: "))
-    return opção
-def sair():
-    print("Encerrando o programa...")
-escolha = menu_principal()
+    while True:
+        print("\n*Calculadora de Conversão de Unidades*")
+        print("1. Converter de centímetros para metros")
+        print("2. Converter de metros para centímetros")
+        print("3. Converter de celsius para fahrenheit")
+        print("4. Converter de fahrenheit para celsius")   
+        print("5. Sair")
+        opção = str(input("Digite a opção desejada: "))
+        if opção == "5":
+            break
+        valor = float(input("Digite o seu valor:  "))
+        if opção == "1":
+            valor = ConversorMedidas.cm_to_m(valor)
+        elif opção == "2":
+            valor = ConversorMedidas.m_to_cm(valor)
+        elif opção == "3":
+            valor= ConversorTemperatura.celsius_to_fahrenheit(valor)
+        elif opção == "4":
+            valor= ConversorTemperatura.fahrenheit_to_celsius(valor)
+        else:
+            print("Opção inválida. Tente novamente.")
+        print('Resultado:   ',valor)
 
-if escolha == "1":
-   from conversor_medidas import ConversorMedidas
-elif escolha == "2":
-   from converversor_temperaturas import ConversorTemperatura
-elif escolha == "3":
-    sair()
-else:
-    print("Opção não existente, tente novamente")
+menu_principal()
